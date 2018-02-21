b643eb2635b7e4829c7afe8e754c303c7d9f9924
smilfinken <johan@smilfinken.net>
2018-02-21 18:04:57 +0100

    removed mqtt, refactored

--- Sensors/EnvironmentMonitor/EnvironmentMonitor.ino
+++ Sensors/EnvironmentMonitor/EnvironmentMonitor.ino
@@ -1,71 +1,76 @@
 #include "localconfig.h"
 
-#include <string.h>
-#include <time.h>
+#define MAXTIMEOUT 20 * 1000  // milliseconds (20 s)
+#define MAXMESSAGELENGTH 100  // characters
+#define SLEEPTIME 15 * 60 // seconds (15 m)
+
+#define MQTT_SOCKET_TIMEOUT 3
+
+#ifdef LOCALCONFIG
+const char *ssid = LC_SSID;
+const char *password = LC_SSIDPW;
+const char *reportHost = LC_REPORTHOST;
+const int reportPort = LC_REPORTPORT;
+const char *reportUri = LC_REPORTURI;
+#else
+const char *ssid = "SSID";
+const char *password = "PW";
+const char *reportHost = "webserver.org";
+const int reportPort = 80;
+const char *reportUri = "/report";
+#endif
+
 #include <nvs.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
-#define MAXTIMEOUT 20 * 1000  // milliseconds (20 s)
-#define MAXMESSAGELENGTH 100  // characters
-#define TZOFFSET 1 * 60 * 60  // seconds (2 h)
-#define DSTOFFSET 1 * 60 * 60 // seconds (1 h)
-#define SLEEPTIME 15 * 60 // seconds (15 m)
-
-#ifdef LOCALCONFIG
-const char *ssid     = LC_SSID;
-const char *password = LC_SSIDPW;
-const char *mqttHost = LC_MQTTHOST;
-const int mqttPort = LC_MQTTPORT;
-const char *mqttUser = LC_MQTTUSER;
-const char *mqttPassword = LC_MQTTPW;
-const char *mqttTopic = LC_MQTTTOPIC;
-const char *reportHost = LC_REPORTHOST;
-const int reportPort = LC_REPORTPORT;
-const char *ntpHost = LC_NTPHOST;
-#else
-const char *ssid     = "SSID";
-const char *password = "PW";
-const char *mqttHost = "mqtt.org";
-const int mqttPort = 12345;
-const char *mqttUser = "user";
-const char *mqttPassword = "pw";
-const char *mqttTopic = "esp/test";
-const char *reportHost = "webserver.org";
-const int reportPort = 80;
-const char *ntpHost = "pool.ntp.org";
-#endif
+int start = millis();
+bool serialInitialized = false;
 
 uint32_t nvshandle;
-String id = "";
+char *id = "unknown\0";
 char message[MAXMESSAGELENGTH];
-int start = millis();
+
+Adafruit_BME280 bme(2, 17, 15, 16);
+bool bmeInitialized = false;
 
 void initSerial() {
 #ifdef DEBUG
   Serial.begin(115200);
   delay(50);
+  serialInitialized = true;
 #endif
 }
 
-void printSerial(String message) {
+void printSerial(char *message) {
 #ifdef DEBUG
-  Serial.print(message);
+  if (serialInitialized) {
+    Serial.print(message);
+  }
 #endif
 }
 
-String getChipId() {
+void deepSleep() {
+  int wakeupInterval = ((SLEEPTIME * 1000) - millis()) * 1000;
+  snprintf(message, MAXMESSAGELENGTH, "\nsleeping for %i seconds\n", wakeupInterval / 1000 / 1000);
+  printSerial(message);
+
+  esp_sleep_enable_timer_wakeup(wakeupInterval);
+  esp_deep_sleep_start();
+}
+
+void getChipId(char *const chipId) {
   uint64_t chipIdNum = ESP.getEfuseMac();
-  char mac[12];
-  sprintf(mac, "%" PRIx64, chipIdNum);
-  char chipId[18];
+  char *mac = (char*)malloc(13);
+  snprintf(mac, 13, "%" PRIx64, chipIdNum);
   for (int i = 0; i <= 5; i++) {
     strncpy(&chipId[i * 3], &mac[i * 2], 2);
     chipId[i * 3 + 2] = i < 5 ? ':' : '\0';
   }
-  return String(chipId);
+  free(mac);
 }
 
 bool initNonVolatile() {
@@ -77,42 +82,28 @@ bool initNonVolatile() {
   }
 
   size_t required_size;
-  nvs_get_str(nvshandle, "unitid", NULL, &required_size);
-  char *unitid = (char*)malloc(required_size);
-  if (nvs_get_str(nvshandle, "unitid", unitid, &required_size)) {
-    id = getChipId();
-    snprintf(message, MAXMESSAGELENGTH, "failed to read unit ID from storage, using chip ID %s\n", id.c_str());
+  if (nvs_get_str(nvshandle, "unitid", NULL, &required_size) == ESP_OK) {
+    id = new char(required_size);
+    nvs_get_str(nvshandle, "unitid", id, &required_size);
+    snprintf(message, MAXMESSAGELENGTH, "unit ID %s retrieved from storage\n", id);
     printSerial(message);
   } else {
-    snprintf(message, MAXMESSAGELENGTH, "unit ID %s retrieved from storage\n", unitid);
+    id = (char*)malloc(18);
+    getChipId(id);
+    snprintf(message, MAXMESSAGELENGTH, "failed to read unit ID from storage, using chip ID %s\n", id);
     printSerial(message);
-    id = String(unitid);
   }
 
   return true;
 }
 
-float getTemperature() {
-  float result = 20.1;
-  return result;
-}
-
-time_t getTimestamp() {
-  time_t timestamp;
-  time(&timestamp);
-  return timestamp;
-}
-
-char *getJsonData() {
-  StaticJsonBuffer<400> jsonBuffer;
-  JsonObject &root = jsonBuffer.createObject();
-  root["senderId"] = id;
-  root["temperature"] = getTemperature();
-  root["timestamp"] = getTimestamp();
-  size_t jsonMessageLength = root.measureLength() + 1;
-  char *jsonMessage = (char*)malloc(jsonMessageLength);
-  root.printTo(jsonMessage, jsonMessageLength);
-  return jsonMessage;
+bool initSensors() {
+  if (!bmeInitialized && !bme.begin()) {
+    printSerial("unable to initialize BME280\n");
+    return false;
+  }
+  bmeInitialized = true;
+  return bmeInitialized;
 }
 
 bool connectWifi() {
@@ -137,81 +128,60 @@ bool connectWifi() {
   return true;
 }
 
-bool configureTime() {
-  printSerial("configuring time...");
-
-  struct tm timeInfo;
-  configTime(TZOFFSET, DSTOFFSET, ntpHost);
-  while (!getLocalTime(&timeInfo)) {
-    if (millis() - start > MAXTIMEOUT) {
-      printSerial(" failed to sync time\n");
-      return false;
-    }
-    delay(10);
-    printSerial(".");
-  }
-
-  strftime(message, MAXMESSAGELENGTH, " done, time is %c\n", &timeInfo);
-  printSerial(message);
-
-  return true;
+float getTemperature() {
+  return bmeInitialized ? bme.readTemperature() : 0.0f;
 }
 
-bool publishSensorReadings() {
-  WiFiClient wifiClient;
-  PubSubClient mqttClient(wifiClient);
-  mqttClient.setServer(mqttHost, mqttPort);
+int getPressure() {
+  return bmeInitialized ? bme.readPressure() : 0;
+}
 
-  snprintf(message, MAXMESSAGELENGTH, "connecting to MQTT at %s:%d...", mqttHost, mqttPort);
-  printSerial(message);
-  
-  while (!mqttClient.connected()) {
-    if (millis() - start > MAXTIMEOUT) {
-      snprintf(message, MAXMESSAGELENGTH, " failed with state %d\n", mqttClient.state());
-      printSerial(message);
-      return false;
-    }
+int getHumidity() {
+  return bmeInitialized ? bme.readHumidity() : 0;
+}
 
-    if (mqttClient.connect(id.c_str(), mqttUser, mqttPassword )) {
-      printSerial(" connected, sending message\n");
-      bool result = mqttClient.publish(mqttTopic, getJsonData());
-      mqttClient.disconnect();
-      return result;
-    } else {
-      delay(200);
-      printSerial(".");
-    }
-  }
-
-  return false;
+char *getJsonData() {
+  StaticJsonBuffer<512> jsonBuffer;
+  JsonObject &root = jsonBuffer.createObject();
+  root["sensorId"] = id;
+  root["temperature"] = getTemperature();
+  root["pressure"] = getPressure();
+  root["humidity"] = getHumidity();
+  size_t jsonMessageLength = root.measureLength() + 1;
+  char *jsonMessage = (char*)malloc(jsonMessageLength);
+  root.printTo(jsonMessage, jsonMessageLength);
+  return jsonMessage;
 }
 
 bool reportSensorReadings() {
-  snprintf(message, MAXMESSAGELENGTH, "connecting to %s:%d...", reportHost, reportPort);
+  bool result = false;
+  
+  snprintf(message, MAXMESSAGELENGTH, "connecting to %s:%d%s...", reportHost, reportPort, reportUri);
   printSerial(message);
 
   HTTPClient httpClient;
-  httpClient.begin(reportHost, reportPort);
+  httpClient.begin(reportHost, reportPort, reportUri);
   httpClient.addHeader("Content-Type", "application/json");
-  bool result = (httpClient.POST(getJsonData()) > 0);
+  int responseCode = httpClient.POST(getJsonData());
+  if (responseCode == 200) {
+    snprintf(message, MAXMESSAGELENGTH, " success, response code %d\n%s", responseCode, httpClient.getString().c_str());
+    printSerial(message);
+    result = true;
+  } else if (responseCode > 0) {
+    snprintf(message, MAXMESSAGELENGTH, " failed, response code %d\n%s", responseCode, httpClient.getString().c_str());
+    printSerial(message);
+  } else {
+    snprintf(message, MAXMESSAGELENGTH, " failed, response code %d\n", responseCode);
+    printSerial(message);
+  }
   httpClient.end();
   return result;
-}
-
-void deepSleep() {
-  int wakeupInterval = ((SLEEPTIME * 1000) - millis()) * 1000;
-  snprintf(message, MAXMESSAGELENGTH, "\nsleeping for %i seconds", wakeupInterval / 1000 / 1000);
-  printSerial(message);
-
-  esp_sleep_enable_timer_wakeup(wakeupInterval);
-  esp_deep_sleep_start();
 }
 
 void setup()
 {
   initSerial();
-  if (initNonVolatile() && connectWifi() && configureTime()) {
-    publishSensorReadings();
+  if (initSensors() && initNonVolatile() && connectWifi()) {
     reportSensorReadings();
   }
   deepSleep();
