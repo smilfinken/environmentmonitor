8ebb29bbded5314dca329808e5c24db7677439bb
smilfinken <johan@smilfinken.net>
2018-02-25 18:50:58 +0100

    dust sensor, activity indicator

--- Sensors/EnvironmentMonitor/EnvironmentMonitor.ino
+++ Sensors/EnvironmentMonitor/EnvironmentMonitor.ino
@@ -2,9 +2,7 @@
 
 #define MAXTIMEOUT 20 * 1000  // milliseconds (20 s)
 #define MAXMESSAGELENGTH 100  // characters
-#define SLEEPTIME 15 * 60 // seconds (15 m)
-
-#define MQTT_SOCKET_TIMEOUT 3
+#define SLEEPTIME 20 * 60 // seconds (20 m)
 
 #ifdef LOCALCONFIG
 const char *ssid = LC_SSID;
@@ -15,7 +13,7 @@ const char *reportUri = LC_REPORTURI;
 #else
 const char *ssid = "SSID";
 const char *password = "PW";
-const char *reportHost = "webserver.org";
+const char *reportHost = "webserver.invalid;
 const int reportPort = 80;
 const char *reportUri = "/report";
 #endif
@@ -34,8 +32,24 @@ uint32_t nvshandle;
 char *id = "unknown\0";
 char message[MAXMESSAGELENGTH];
 
-Adafruit_BME280 bme(2, 17, 15, 16);
+#define BME_SCK 18
+#define BME_MOSI 19
+#define BME_MISO 23
+#define BME_CS 17
+Adafruit_BME280 bme(BME_SCK, BME_MOSI, BME_MISO, BME_CS);
 bool bmeInitialized = false;
+
+#define ACTIVE_LED 4
+
+#define ANALOG_MAXVALUE 4095.0
+#define DUST_READ 32
+#define DUST_LED 16
+#define DUST_SAMPLETIME 280
+#define DUST_PULSETIME 320
+#define DUST_CYCLETIME 10000
+
+#define MAXLOOPCOUNT 10
+float ppm = 0.0;
 
 void initSerial() {
 #ifdef DEBUG
@@ -53,11 +67,20 @@ void printSerial(char *message) {
 #endif
 }
 
+void showActivity() {
+  digitalWrite(ACTIVE_LED, LOW);
+}
+
+void showSleep() {
+  digitalWrite(ACTIVE_LED, HIGH);
+}
+
 void deepSleep() {
   int wakeupInterval = ((SLEEPTIME * 1000) - millis()) * 1000;
   snprintf(message, MAXMESSAGELENGTH, "\nsleeping for %i seconds\n", wakeupInterval / 1000 / 1000);
   printSerial(message);
 
+  showSleep();
   esp_sleep_enable_timer_wakeup(wakeupInterval);
   esp_deep_sleep_start();
 }
@@ -97,7 +120,14 @@ bool initNonVolatile() {
   return true;
 }
 
+void initIO() {
+  pinMode(ACTIVE_LED, OUTPUT);
+  pinMode(DUST_LED, OUTPUT);
+  pinMode(DUST_READ, INPUT);
+}
+
 bool initSensors() {
+  showActivity();
   if (!bmeInitialized && !bme.begin()) {
     printSerial("unable to initialize BME280\n");
     return false;
@@ -140,6 +170,10 @@ int getHumidity() {
   return bmeInitialized ? bme.readHumidity() : 0;
 }
 
+float getParticleCount() {
+  return readDustSensor() / (float)MAXLOOPCOUNT;
+}
+
 char *getJsonData() {
   StaticJsonBuffer<512> jsonBuffer;
   JsonObject &root = jsonBuffer.createObject();
@@ -147,6 +181,7 @@ char *getJsonData() {
   root["temperature"] = getTemperature();
   root["pressure"] = getPressure();
   root["humidity"] = getHumidity();
+  root["particles"] = getParticleCount();
   size_t jsonMessageLength = root.measureLength() + 1;
   char *jsonMessage = (char*)malloc(jsonMessageLength);
   root.printTo(jsonMessage, jsonMessageLength);
@@ -155,7 +190,7 @@ char *getJsonData() {
 
 bool reportSensorReadings() {
   bool result = false;
-  
+
   snprintf(message, MAXMESSAGELENGTH, "connecting to %s:%d%s...", reportHost, reportPort, reportUri);
   printSerial(message);
 
@@ -164,7 +199,7 @@ bool reportSensorReadings() {
   httpClient.addHeader("Content-Type", "application/json");
   int responseCode = httpClient.POST(getJsonData());
   if (responseCode == 200) {
-    snprintf(message, MAXMESSAGELENGTH, " success, response code %d\n%s", responseCode, httpClient.getString().c_str());
+    snprintf(message, MAXMESSAGELENGTH, " success, response code %d\n\n%s\n", responseCode, httpClient.getString().c_str());
     printSerial(message);
     result = true;
   } else if (responseCode > 0) {
@@ -178,9 +213,44 @@ bool reportSensorReadings() {
   return result;
 }
 
+void dustLedOn() {
+  digitalWrite(DUST_LED, LOW);
+}
+
+void dustLedOff() {
+  digitalWrite(DUST_LED, HIGH);
+}
+
+float getDustSensorSample() {
+  dustLedOn();
+  delayMicroseconds(DUST_SAMPLETIME);
+  int raw = analogRead(DUST_READ);
+  delayMicroseconds(DUST_PULSETIME - DUST_SAMPLETIME);
+  dustLedOff();
+  float voltage = 3.3 * (raw / (float)ANALOG_MAXVALUE);
+  float density = fmin(0.0, 0.17 * voltage - 0.1);
+  //snprintf(message, MAXMESSAGELENGTH, "[ r = %d, v = %f, d = %f ]\n", raw, voltage, density);
+  //printSerial(message);
+  return density;
+}
+
+float readDustSensor() {
+  //printSerial("activating dust sensor\n");
+  getDustSensorSample(); // warm up, first read is always 0
+  delay(10);
+  for (int i = 0; i <= MAXLOOPCOUNT; i++) {
+    ppm += getDustSensorSample();
+    delayMicroseconds(DUST_CYCLETIME - DUST_PULSETIME);
+  }
+  //snprintf(message, MAXMESSAGELENGTH, "dust density value is %f after %d loops\n", ppm, MAXLOOPCOUNT);
+  //printSerial(message);
+  return ppm;
+}
+
 void setup()
 {
   initSerial();
+  initIO();
   if (initSensors() && initNonVolatile() && connectWifi()) {
     reportSensorReadings();
   }
